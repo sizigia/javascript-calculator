c7515e4bbb107a479b168c286bd11e32979ad3a6
sizigia <cumbresdigitales@gmail.com>
2019-05-06 16:35:38 -0300

    add a calc object to store the variables I need to perform operations

--- js/calc.js
+++ js/calc.js
@@ -1,17 +1,22 @@
-
+const calc = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
 
 function add(a, b) {
-    return a + b;
+    return displayText.innerHTML = (a + b);
 }
 
 function sub(a, b) {
-    return a - b;
+    return displayText.innerHTML = (a - b);
 }
 
 function mul(a, b) {
-    return a * b;
+    return displayText.innerHTML = (a * b);
 }
 
 function div(a, b) {
-    return a / b;
+    return displayText.innerHTML = (a / b);
 }
